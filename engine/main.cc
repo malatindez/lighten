e0894ad56f5ee04704d32072b9cbd838c0878e0c
malatindez <53783400+malatindez@users.noreply.github.com>
2022-04-28 20:44:59 +0300

    Update main.cc

--- engine/main.cc
+++ engine/main.cc
@@ -1,18 +1,33 @@
-// include the basic windows header file
-#include <windows.h>
-#include <windowsx.h>
+#include <Windows.h>
+
+#include "math/ray.hpp"
+#include "math/vec.hpp"
 
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam,
                             LPARAM lParam);
 
-#include "math/ray.hpp"
-#include "math/vec.hpp"
 using namespace math;
+
+const ivec2 kWindowPosition{0};
+const ivec2 kWindowResolution{1280, 720};
+
+bool hit_sphere(vec3 const& center, float radius, ray const& r) {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0f * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius * radius;
+  float discriminant = b * b - 4 * a * c;
+  return discriminant > 0;
+}
+
 vec3 color(ray const& r) {
+  if (hit_sphere(vec3{0, 0, -1}, 0.5f, r)) {
+    return vec3{1, 0, 0};
+  }
   vec3 unit_direction = r.direction().unit_vector();
-  float t = 0.5 * (unit_direction.y() + 1);
-  return (1.0 - t) * vec3{1} + t * vec3{0.5, 0.7, 1.0};
+  float t = 0.5f * (unit_direction.y() + 1);
+  return (1.0f - t) * vec3{1} + t * vec3{0.5f, 0.7f, 1.0f};
 }
 
 void draw(HDC hdc, ivec2 window_size) {
@@ -22,24 +37,21 @@ void draw(HDC hdc, ivec2 window_size) {
   vec3 origin{0};
   for (int j = window_size.y() - 1; j >= 0; j--) {
     for (int i = 0; i < window_size.x(); i++) {
-      float u = float(i) / window_size.x();
-      float v = float(j) / window_size.y();
+      float u = float(i) / float(window_size.x());
+      float v = float(j) / float(window_size.y());
       ray r(origin, lower_left_corner + u * horizontal + v * vertical);
       vec3 col = color(r);
-      int ir = int(255.99 * col[0]);
-      int ig = int(255.99 * col[1]) << 8;
-      int ib = int(255.99 * col[2]) << 16;
+      auto ir = int(255.99 * col[0]);
+      auto ig = int(255.99 * col[1]) << 8;
+      auto ib = int(255.99 * col[2]) << 16;
 
       SetPixel(hdc, i, j, ir | ig | ib);
     }
   }
 }
-const ivec2 kWindowPosition{0};
-const ivec2 kWindowResolution{1280, 720};
 
 // the entry point for any Windows program
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nShowCmd) {
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, INT nShowCmd) {
   // the handle for the window, filled by a function
   HWND hWnd;
   // this struct holds information for the window class
@@ -53,7 +65,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   wc.style = CS_HREDRAW | CS_VREDRAW;
   wc.lpfnWndProc = WindowProc;
   wc.hInstance = hInstance;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
   wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
   wc.lpszClassName = L"WindowClass1";
 
@@ -69,10 +81,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                          kWindowPosition.y(),    // y-position of the window
                          kWindowResolution.x(),  // width of the window
                          kWindowResolution.y(),  // height of the window
-                         NULL,       // we have no parent window, NULL
-                         NULL,       // we aren't using menus, NULL
+                         nullptr,    // we have no parent window, nullptr
+                         nullptr,    // we aren't using menus, nullptr
                          hInstance,  // application handle
-                         NULL);      // used with multiple windows, NULL
+                         nullptr);   // used with multiple windows, nullptr
 
   // display the window on the screen
   ShowWindow(hWnd, nShowCmd);
@@ -84,7 +96,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   // this struct holds Windows event messages
   MSG msg;
   // wait for the next message in the queue, store the result in 'msg'
-  while (GetMessage(&msg, NULL, 0, 0)) {
+  while (GetMessage(&msg, nullptr, 0, 0)) {
     // translate keystroke messages into the right format
     TranslateMessage(&msg);
 
@@ -93,20 +105,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
   }
 
   // return this part of the WM_QUIT message to Windows
-  return msg.wParam;
+  return int(msg.wParam);
 }
 
 // this is the main message handler for the program
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam,
                             LPARAM lParam) {
-  // sort through and find what code to run for the message given
-  switch (message) {
-    // this message is read when the window is closed
-    case WM_DESTROY: {
-      // close the application entirely
-      PostQuitMessage(0);
-      return 0;
-    } break;
+  if (message == WM_DESTROY) {
+    PostQuitMessage(0);
+    return 0;
   }
 
   // Handle any messages the switch statement didn't
