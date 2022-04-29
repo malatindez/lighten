4c0f5b586a8ca06e07daf5e0630a7c38282f3443
malatindez <53783400+malatindez@users.noreply.github.com>
2022-04-29 12:38:28 +0300

    refactor window classes
    move functions to the .cpp file

--- engine/misc/bitmap-window.hpp
+++ engine/misc/bitmap-window.hpp
@@ -1,69 +1,41 @@
 #pragma once
 #include <exception>
 
-#include "window.hpp"
+#include "misc/window.hpp"
 namespace engine {
 class BitmapWindow final : public Window {
- public:
-  BitmapWindow(WindowClass const& window_class, DWORD extended_style,
-               std::wstring const& class_name, std::wstring const& window_name,
+public:
+  BitmapWindow(WindowClass const &window_class, DWORD extended_style,
+               std::wstring const &class_name, std::wstring const &window_name,
                DWORD style, math::ivec2 position, math::ivec2 size,
                HWND parent_window, HMENU menu, HINSTANCE instance,
-               LPVOID lp_param)
-      : Window(window_class, extended_style, class_name, window_name, style,
-               position, size, parent_window, menu, instance, lp_param) {
-    bitmap_.resize(size_t(size.x()) * size.y());
+               LPVOID lp_param);
 
-    Window::SetMainLoopCallback(std::bind_front(&BitmapWindow::MainLoop, this));
+  BitmapWindow(BitmapWindow &&BitmapWindow) = delete;
+  BitmapWindow &operator=(BitmapWindow &&BitmapWindow) = delete;
+  BitmapWindow(BitmapWindow const &BitmapWindow) = delete;
+  BitmapWindow &operator=(BitmapWindow const &BitmapWindow) = delete;
 
-    hdc_ = GetDC(handle());
-
-    bitmap_info_.bmiHeader.biSize = sizeof(bitmap_info_.bmiHeader);
-    bitmap_info_.bmiHeader.biPlanes = 1;
-    bitmap_info_.bmiHeader.biBitCount = 32;
-    bitmap_info_.bmiHeader.biCompression = BI_RGB;
-    bitmap_info_.bmiHeader.biWidth = size.x();
-    bitmap_info_.bmiHeader.biHeight = size.y();
-  }
-
-  BitmapWindow(BitmapWindow&& BitmapWindow) = delete;
-  BitmapWindow& operator=(BitmapWindow&& BitmapWindow) = delete;
-  BitmapWindow(BitmapWindow const& BitmapWindow) = delete;
-  BitmapWindow& operator=(BitmapWindow const& BitmapWindow) = delete;
-
-  void SetMainLoopCallback(MainLoopCallback const& callback) override {
+  void SetMainLoopCallback(MainLoopCallback const &callback) override {
     main_loop_callback_ = callback;
   }
   [[nodiscard]] inline BITMAPINFO bitmap_info() const noexcept {
     return bitmap_info_;
   }
 
-  [[nodiscard]] constexpr std::vector<uint32_t>& bitmap() noexcept {
+  [[nodiscard]] constexpr std::vector<uint32_t> &bitmap() noexcept {
     return bitmap_;
   }
-  [[nodiscard]] constexpr std::vector<uint32_t> const& bitmap() const noexcept {
+  [[nodiscard]] constexpr std::vector<uint32_t> const &bitmap() const noexcept {
     return bitmap_;
   }
 
- private:
-  void OnSizeChanged() override {
-    const math::ivec2 size = this->size();
-    bitmap_info_.bmiHeader.biWidth = size.x();
-    bitmap_info_.bmiHeader.biHeight = size.y();
-    bitmap_.resize(size_t(size.x()) * size.y());
-  }
-  void MainLoop(Window&) {
-    if (main_loop_callback_ != nullptr) {
-      main_loop_callback_(std::ref(*this));
-    }
-    // draw bitmap
-    StretchDIBits(hdc_, 0, 0, size().x(), size().y(), 0, 0, size().x(),
-                  size().y(), bitmap_.data(), &bitmap_info_, DIB_RGB_COLORS,
-                  SRCCOPY);
-  }
+private:
+  void OnSizeChanged() override;
+  void MainLoop(Window &);
   HDC hdc_;
   std::vector<uint32_t> bitmap_;
   BITMAPINFO bitmap_info_;
   MainLoopCallback main_loop_callback_;
 };
-}  // namespace engine
+} // namespace engine
--- engine/misc/window.hpp
+++ engine/misc/window.hpp
@@ -9,54 +9,32 @@ using WindowClass = WNDCLASSEXW;
 class Window;
 
 using WindowCallback =
-    std::function<LRESULT(Window&, HWND, UINT, WPARAM, LPARAM)>;
+    std::function<LRESULT(Window &, HWND, UINT, WPARAM, LPARAM)>;
 
-using MainLoopCallback = std::function<void(Window&)>;
+using MainLoopCallback = std::function<void(Window &)>;
 
 // Simple WINAPI window wrapper
 class Window {
- public:
-  Window(WindowClass const& window_class_template, DWORD extended_style,
-         std::wstring const& class_name, std::wstring const& window_name,
+public:
+  Window(WindowClass const &window_class_template, DWORD extended_style,
+         std::wstring const &class_name, std::wstring const &window_name,
          DWORD style, math::ivec2 position, math::ivec2 size,
-         HWND parent_window, HMENU menu, HINSTANCE instance, LPVOID lp_param)
-      : position_(position), size_(size) {
-    // Create custom window_class for this window
-    WindowClass window_class{window_class_template};
-    window_class.lpszClassName = class_name.c_str();
-    window_class.lpfnWndProc = Window::StaticWindowProc;
-    window_class.cbSize = sizeof(WindowClass);
-    window_class.cbWndExtra = sizeof(void*);
-
-    RegisterClassExW(&window_class);
-    handle_ =
-        CreateWindowExW(extended_style, class_name.c_str(), window_name.c_str(),
-                        style, position.x(), position.y(), size.x(), size.y(),
-                        parent_window, menu, instance, lp_param);
-    // save window handle in the window class
-    SetWindowLongPtrW(handle_, 0, reinterpret_cast<LONG_PTR>(this));
-
-    if (!handle_) {
-      MessageBoxW(nullptr, L"CreateWindowEx failed", nullptr, 0);
-      UnregisterClassW(class_name.c_str(), instance);
-      throw GetLastError();
-    }
-  }
-  // remove copy and move semantics because the callback system is bound to the address of the window
-  Window(Window&& Window) = delete;
-  Window& operator=(Window&& Window) = delete;
-  Window(Window const& Window) = delete;
-  Window& operator=(Window const& Window) = delete;
+         HWND parent_window, HMENU menu, HINSTANCE instance, LPVOID lp_param);
+  // remove copy and move semantics because the callback system is bound to the
+  // address of the window
+  Window(Window &&Window) = delete;
+  Window &operator=(Window &&Window) = delete;
+  Window(Window const &Window) = delete;
+  Window &operator=(Window const &Window) = delete;
 
   virtual ~Window() { DestroyWindow(handle_); }
 
   // Windows event callback
-  virtual void SetCallback(UINT message, WindowCallback const& function) {
+  virtual void SetCallback(UINT message, WindowCallback const &function) {
     callbacks_.try_emplace(message, function);
   }
 
-  // Main loop callback
-  virtual void SetMainLoopCallback(MainLoopCallback const& callback) {
+  virtual void SetMainLoopCallback(MainLoopCallback const &callback) {
     main_loop_callback_ = callback;
   }
 
@@ -66,54 +44,17 @@ class Window {
     return position_;
   }
 
-  void StartMainLoop() {
-    bool running = true;
-    while (running) {
-      MSG msg;
-      while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
-        if (msg.message == WM_QUIT) {
-          running = false;
-        }
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-      }
-      if (main_loop_callback_ != nullptr) {
-        main_loop_callback_(std::ref(*this));
-      }
-    }
-  }
+  void StartMainLoop();
 
- protected:
+protected:
   virtual void OnSizeChanged() {}
 
- private:
+private:
   LRESULT CALLBACK WindowProcCallback(HWND handle, UINT message, WPARAM w_param,
-                                      LPARAM l_param) {
-    // update the window size if it has changed
-    if (message == WM_SIZE) {
-      size_ = math::ivec2{LOWORD(l_param), HIWORD(l_param)};
-      OnSizeChanged();
-    }  // update window position if it has changed
-    else if (message == WM_WINDOWPOSCHANGED) {
-      auto window_pos = reinterpret_cast<LPWINDOWPOS>(l_param);
-      position_ = math::ivec2{window_pos->x, window_pos->y};
-    }
-    // call callback if it exists
-    if (auto it = callbacks_.find(message); it != callbacks_.end()) {
-      return it->second(std::ref(*this), handle, message, w_param, l_param);
-    }
-    // handle any messages that callbacks didnt
-    return DefWindowProcW(handle, message, w_param, l_param);
-  }
+                                      LPARAM l_param);
 
   static LRESULT CALLBACK StaticWindowProc(HWND handle, UINT message,
-                                           WPARAM w_param, LPARAM l_param) {
-    if (auto ptr = reinterpret_cast<Window*>(GetWindowLongPtrW(handle, 0));
-        ptr != nullptr) {
-      return ptr->WindowProcCallback(handle, message, w_param, l_param);
-    }
-    return DefWindowProcW(handle, message, w_param, l_param);
-  }
+                                           WPARAM w_param, LPARAM l_param);
 
   HWND handle_;
   std::unordered_map<UINT, WindowCallback> callbacks_;
@@ -121,4 +62,4 @@ class Window {
   math::ivec2 position_;
   math::ivec2 size_;
 };
-};  // namespace engine
+}; // namespace engine
