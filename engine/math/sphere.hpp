1fb573605ead74034bb039cc636d1410c653c456
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-04 11:36:00 +0300

    Update sphere.hpp

--- engine/math/sphere.hpp
+++ engine/math/sphere.hpp
@@ -5,12 +5,12 @@ namespace engine::math
     class Sphere
     {
     public:
-        Sphere(math::vec3 center, float radius) : center_(center), radius_(radius) {}
-        bool Hit(math::Ray const &r)
+        Sphere(vec3 center, float radius) : center_(center), radius_(radius) {}
+        bool Hit(Ray const &r)
 
             const noexcept
         {
-            math::vec3 oc = r.origin() - center_;
+            vec3 oc = r.origin() - center_;
             // find sin of an angle between OC and d, where OC is the vector between ray
             // origin and the center of the sphere, and d is the direction of the ray
             float sinAngle =
@@ -20,12 +20,12 @@ namespace engine::math
             return sinAngle * oc.length() <= radius_;
         }
         [[nodiscard]] inline float radius() const noexcept { return radius_; }
-        [[nodiscard]] inline math::vec3 center() const noexcept { return center_; }
+        [[nodiscard]] inline vec3 center() const noexcept { return center_; }
         void SetRadius(float radius) noexcept { radius_ = radius; }
-        void SetCenter(math::vec3 center) noexcept { center_ = center; }
+        void SetCenter(vec3 center) noexcept { center_ = center; }
 
     private:
-        math::vec3 center_;
+        vec3 center_;
         float radius_;
     };
 } // namespace engine::math
