c8ded2c9b2a34cf2fd0b230b0226c12c49f81ef4
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-13 00:19:11 +0300

    Update controller

--- project/controller.cpp
+++ project/controller.cpp
@@ -2,99 +2,74 @@
 #include "core/application.hpp"
 using namespace engine;
 using namespace engine::math;
-Controller::Controller(BitmapWindow &window, std::shared_ptr<Scene> scene) : scene_(scene), window_(window)
-{
-    window_.SetCallback(WM_KEYDOWN, std::bind_front(&Controller::OnKeyDown, this));
-    window_.SetCallback(WM_KEYUP, std::bind_front(&Controller::OnKeyRelease, this));
-    window_.SetCallback(WM_RBUTTONDOWN, std::bind_front(&Controller::OnRButtonDown, this));
-    window_.SetCallback(WM_RBUTTONUP, std::bind_front(&Controller::OnRButtonUp, this));
-    window_.SetCallback(WM_LBUTTONDOWN, std::bind_front(&Controller::OnLButtonDown, this));
-    window_.SetCallback(WM_LBUTTONUP, std::bind_front(&Controller::OnLButtonUp, this));
-    window_.SetCallback(WM_EXITSIZEMOVE, std::bind_front(&Controller::OnExitSizeMove, this));
-    window_.SetCallback(WM_DESTROY, std::bind_front(&Controller::OnDestroy, this));
-}
-LRESULT Controller::OnKeyDown(Window &, HWND handle, UINT message,
-                              WPARAM w_param, LPARAM l_param)
-{
-    key_states_[w_param] = true;
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
-LRESULT Controller::OnKeyRelease(Window &, HWND handle, UINT message,
-                                 WPARAM w_param, LPARAM l_param)
-{
-    key_states_[w_param] = false;
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
-LRESULT Controller::OnRButtonDown(Window &, HWND handle, UINT message,
-                                  WPARAM w_param, LPARAM l_param)
-{
-    ShowCursor(false);
-    rbuttonstate_ = true;
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
-LRESULT Controller::OnRButtonUp(Window &, HWND handle, UINT message,
-                                WPARAM w_param, LPARAM l_param)
-{
-    ShowCursor(true);
-    rbuttonstate_ = false;
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
-LRESULT Controller::OnLButtonDown(Window &, HWND handle, UINT message,
-                                  WPARAM w_param, LPARAM l_param)
-{
-    lbuttonstate_ = true;
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
-LRESULT Controller::OnLButtonUp(Window &, HWND handle, UINT message,
-                                WPARAM w_param, LPARAM l_param)
-{
-    lbuttonstate_ = false;
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
 
-LRESULT Controller::OnExitSizeMove(Window &, HWND handle, UINT message,
-                                   WPARAM w_param, LPARAM l_param) const
+Controller::Controller(BitmapWindow &window, std::shared_ptr<Scene> scene) : scene_(scene), window_(window) {}
+void Controller::OnEvent(engine::Event &event)
 {
-    scene_->UpdateScene();
-    return DefWindowProcW(handle, message, w_param, l_param);
+    if (event.in_category(EventCategoryApplication))
+    {
+        if (event.type() == EventType::AppUpdate)
+        {
+            window_.PeekOSMessages();
+        }
+        else if (event.type() == EventType::AppTick)
+        {
+            auto const &ape = dynamic_cast<AppTickEvent &>(event);
+            Tick(ape.delta_time());
+        }
+        else if (event.type() == EventType::AppRender)
+        {
+            scene_->Draw(window_);
+        }
+        else if (event.type() == EventType::WindowClose)
+        {
+            Application::Exit();
+        }
+    }
+    else if (event.in_category(EventCategoryInput))
+    {
+        if (event.in_category(EventCategoryMouse))
+        {
+            if (event.type() == EventType::MouseButtonPressed)
+            {
+                ShowCursor(false);
+            }
+            else if (event.type() == EventType::MouseButtonPressed)
+            {
+                ShowCursor(true);
+            }
+        }
+        input_.OnEvent(event);
+    }
 }
-LRESULT Controller::OnDestroy(Window &, HWND handle, UINT message,
-                              WPARAM w_param, LPARAM l_param) const
-{
-    Application::Get().Exit();
-    return DefWindowProcW(handle, message, w_param, l_param);
-}
-
 const vec3 kUp{0, 1, 0};
 const vec3 kDown{0, -1, 0};
 const vec3 kLeft{-1, 0, 0};
 const vec3 kRight{1, 0, 0};
 
-bool Controller::Update(float delta_time)
+void Controller::Tick(float delta_time) const
 {
-    if (!rbuttonstate_)
+    if (!input_.rbutton_down())
     {
-
         vec3 vec{0};
-        if (key_states_['W'])
+        if (input_.key_state('W'))
         {
             vec += kUp;
         }
-        if (key_states_['A'])
+        if (input_.key_state('A'))
         {
             vec += kLeft;
         }
-        if (key_states_['S'])
+        if (input_.key_state('S'))
         {
             vec += kDown;
         }
-        if (key_states_['D'])
+        if (input_.key_state('D'))
         {
             vec += kRight;
         }
-
         scene_->sphere.SetCenter(scene_->sphere.center() + vec * delta_time);
-
+        
         // update the scene if position of the sphere has changed
         if (squared_length(vec) != 0)
         {
@@ -108,17 +83,13 @@ bool Controller::Update(float delta_time)
         POINT point;
         GetCursorPos(&point);
 
-        vec2 diff = vec2{float(point.x) - float(middle.x),
-                         float(middle.y) - float(point.y)} /
-                    length(window_.size());
+        vec2 diff = vec2{ float(point.x) - float(middle.x),
+                         float(middle.y) - float(point.y) } /
+            length(window_.size());
 
         scene_->sphere.SetCenter(scene_->sphere.center() + vec3{diff.x, diff.y, 0});
 
         SetCursorPos(middle.x, middle.y);
-
         scene_->UpdateScene();
     }
-
-    scene_->Draw(window_);
-    return true;
 }
--- project/controller.hpp
+++ project/controller.hpp
@@ -1,48 +1,23 @@
 #pragma once
-#include "interfaces/updatable.hpp"
-#include "render/scene.hpp"
+#include "misc/scene.hpp"
 #include "core/window.hpp"
+#include "core/layer.hpp"
+#include "input.hpp"
 
 #include <memory>
 #include <unordered_map>
 
-class Controller : public engine::interfaces::Updatable
+class Controller : public engine::Layer
 {
 public:
     Controller(engine::BitmapWindow &window, std::shared_ptr<engine::Scene> scene);
-
-    [[nodiscard]] inline bool key_state(uint64_t key) const noexcept
-    {
-        return key_states_.at(key);
-    }
-    [[nodiscard]] constexpr bool lbutton_down() const noexcept
-    {
-        return lbuttonstate_;
-    }
-    [[nodiscard]] constexpr bool rbutton_down() const noexcept
-    {
-        return rbuttonstate_;
-    }
-
-    bool Update(float delta_time) override;
-
+   
+    void OnEvent(engine::Event &event) override;
+ 
 private:
-    LRESULT OnKeyDown(engine::Window &, HWND, UINT, WPARAM, LPARAM);
-    LRESULT OnKeyRelease(engine::Window &, HWND, UINT, WPARAM, LPARAM);
+    void Tick(float delta_time) const;
 
-    LRESULT OnRButtonDown(engine::Window &, HWND, UINT, WPARAM, LPARAM);
-    LRESULT OnRButtonUp(engine::Window &, HWND, UINT, WPARAM, LPARAM);
-
-    LRESULT OnLButtonDown(engine::Window &, HWND, UINT, WPARAM, LPARAM);
-    LRESULT OnLButtonUp(engine::Window &, HWND, UINT, WPARAM, LPARAM);
-
-    LRESULT OnExitSizeMove(engine::Window &, HWND, UINT, WPARAM, LPARAM) const;
-
-    LRESULT OnDestroy(engine::Window &, HWND, UINT, WPARAM, LPARAM) const;
-
-    std::unordered_map<uint64_t, bool> key_states_;
-    bool lbuttonstate_ = false;
-    bool rbuttonstate_ = false;
+    Input input_;
     std::shared_ptr<engine::Scene> scene_;
     engine::BitmapWindow &window_;
 };
