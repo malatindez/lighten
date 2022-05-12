7940efd9d9fd59ded9b343919c752b45293db3c3
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-13 00:17:53 +0300

    Add layer interface

---
+++ engine/core/layer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "core/events/event.hpp"
+
+#include <string>
+
+namespace engine
+{
+  class Layer
+  {
+  public:
+    explicit Layer(std::string_view name = "Layer") : debug_name_(name) {}
+    virtual ~Layer() = default;
+
+    virtual void OnAttach() {}
+    virtual void OnDetach() {}
+    virtual void OnEvent(Event &event) {}
+
+    [[nodiscard]] std::string_view name() const { return debug_name_; }
+
+  private:
+    std::string debug_name_;
+  };
+}
