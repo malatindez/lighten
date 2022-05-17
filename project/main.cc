e8e814802408b9eebebe5f95abbbf7f1acf85b3b
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-17 03:00:24 +0300

    Update main.cc

--- project/main.cc
+++ project/main.cc
@@ -2,7 +2,10 @@
 
 #include "controller.hpp"
 #include "core/application.hpp"
+#include "components/sphere.hpp"
+
 using namespace engine;
+using namespace components;
 
 // the entry point for any Windows program
 INT WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
@@ -35,33 +38,51 @@ INT WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
     // display the window on the screen
     ShowWindow(bmwindow->handle(), cmd_show);
 
+    
     auto scene = std::make_shared<Scene>();
+    entt::registry& registry = scene->registry;
 
-    scene->sphere = math::Sphere{math::vec3{0, -1, -2}, 0.5f};
+    entt::entity sphere = registry.create();
+    registry.emplace<Sphere>(sphere);
+    Transform& sphere_transform = registry.emplace<Transform>(sphere);
+    sphere_transform.position = math::vec3{ 0,-1,-2 };
+    sphere_transform.scale = math::vec3{ 0.5f };
 
-    engine::components::PointLight point_light;
-    point_light.position = math::vec3{ 0,0.25f,0 };
-    point_light.color = engine::math::vec3{ 0.5f,0.0f,0.5f };
+    entt::entity point_light_entity = registry.create();
+    
+    PointLight &point_light = registry.emplace<PointLight>(point_light_entity);
+    Transform &pl_transform = registry.emplace<Transform>(point_light_entity);
+    pl_transform.position = math::vec3 {0,-0.25f,0};
+    point_light.color = math::vec3{ 0.5f,0.0f,0.5f };
     point_light.ambient_intensity = 0.2f;
     point_light.diffuse_intensity = 0.3f;
     point_light.specular_intensity = 0.1f;
-    point_light.attenuation = engine::components::PointLight::Attenuation
+    point_light.attenuation = PointLight::Attenuation
     {
         .constant = 1.0f,
         .linear = 0.7f,
         .quadratic = 0.44f
     };
 
-    engine::components::DirectionalLight directional_light;
+    DirectionalLight directional_light;
     directional_light.direction = math::normalize(math::vec3{ 0,-1, 0 });
-    directional_light.color = engine::math::vec3{ 1,1,0.7f };
+    directional_light.color = math::vec3{ 1,1,0.7f };
     directional_light.ambient_intensity = 0.2f;
     directional_light.diffuse_intensity = 0.3f;
     directional_light.specular_intensity = 0.1f;
 
-    scene->point_lights.push_back( point_light );
-    scene->directional_lights.push_back(directional_light);
     auto controller = std::make_shared<Controller>(*bmwindow, scene);
+
+    registry.emplace<DirectionalLight>(registry.create(), directional_light);
+ //   math::Intersection nearest;
+ //   math::Ray ray;
+ //   nearest.reset();
+  //  auto group = registry.group<Transform, IIntersectable>();
+  // for(auto entity: group)
+ //  {
+ //      //group.get<IIntersectable>(entity)->CheckIntersection(nearest, ray);
+ //  }
+
     
     Application::Init();
 
@@ -72,6 +93,5 @@ INT WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
     application.AddLayer(controller);
     application.Run();
     PostQuitMessage(0);
-
     return 0;
 }
