61625e6408ae04eefe851640860be721c9b5ff1f
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-18 14:49:08 +0300

    Update transform.hpp

--- engine/components/transform.hpp
+++ engine/components/transform.hpp
@@ -5,9 +5,15 @@ namespace engine::components
   class Transform final
   {
   public:
-    math::vec3 position{1};
-    math::vec3 scale{1};
-    math::quat quaternion{ 1,0,0,0 };
+    math::vec3 position;
+    math::vec3 scale;
+    math::quat quaternion;
+    void reset()
+    {
+      position = math::vec3{0};
+      scale = math::vec3{1};
+      quaternion = math::quat{1,0,0,0};
+    }
     [[nodiscard]] math::mat4 model() const noexcept 
     { 
       math::mat4 mat(1);
