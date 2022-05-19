9afad7d24214a364158a5d2a1f0eec82ca063e54
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-19 13:30:31 +0300

    Improve scene renderer perfomance

--- engine/misc/scene.cpp
+++ engine/misc/scene.cpp
@@ -1,11 +1,4 @@
 #include "misc/scene.hpp"
-#include "components/cube.hpp"
-#include "components/directional-light.hpp"
-#include "components/plane.hpp"
-#include "components/point-light.hpp"
-#include "components/sphere.hpp"
-#include "components/spot-light.hpp"
-
 using namespace engine::math;
 
 namespace engine
@@ -20,7 +13,7 @@ namespace engine
     }
     update_scene = false;
     vec3 origin{0};
-    ivec2 window_size = window.size();
+    ivec2 bitmap_size = window.bitmap_size();
     uint32_t *bitmap = window.bitmap().data();
     vec4 BL{-1, -1, 1, 1}; 
     vec4 BR{1, -1, 1, 1}; 
@@ -34,69 +27,52 @@ namespace engine
     math::vec4 BR_BL = BR - BL;
     math::vec4 TL_BL = TL - BL;
 
-    for (int j = window_size.y - 1; j >= 0; j--)
+    auto planes = registry.group<components::Plane>(entt::get<components::Transform>);
+    auto spheres = registry.group<components::Sphere>(entt::get<components::Transform>);
+    auto cubes = registry.group<components::Cube>(entt::get<components::Transform>);
+
+    auto directional_lights = registry.view<components::DirectionalLight>();
+    auto point_lights = registry.group<components::PointLight>(entt::get < components::Transform>);
+    auto spot_lights = registry.group<components::SpotLight>(entt::get < components::Transform>);
+
+    for (int j = 0; j < bitmap_size.y; j++)
     {
-      for (int i = 0; i < window_size.x; i++)
+      for (int i = 0; i < bitmap_size.x; i++)
       {
-        float u = ((static_cast<float>(i)) / static_cast<float>(window_size.x));
-        float v = ((static_cast<float>(j)) / static_cast<float>(window_size.y));
+        float u = ((static_cast<float>(i)) / static_cast<float>(bitmap_size.x));
+        float v = ((static_cast<float>(j)) / static_cast<float>(bitmap_size.y));
         math::vec4 a = BL + BR_BL * u + TL_BL * v;
-        Ray r(origin, normalize(a.as_vec<3>()));
-        
-        auto intersection = FindIntersection(r);
+        Ray ray(origin, normalize(a.as_vec<3>()));
+        math::Intersection intersection;
+        intersection.reset();
+
+        spheres.each([&intersection, &ray](const auto, auto& transform) { components::Sphere::CheckIntersection(transform, intersection, ray); });
+        planes.each([&intersection, &ray](const auto, auto const& plane, auto const& transform) { plane.CheckIntersection(transform, intersection, ray); });
+        cubes.each([&intersection, &ray](const auto, auto& transform) { components::Cube::CheckIntersection(transform, intersection, ray);  });
+
         if (!intersection.exists())
         {
-          bitmap[size_t(j) * window_size.x + i] = 0;
+          bitmap[size_t(j) * bitmap_size.x + i] = 0;
           continue;
         }
 
         components::LightData ld{
             .color = math::vec3{0, 0, 0},
-            .ray = r,
+            .ray = ray,
             .point = intersection.point,
             .normal = intersection.normal,
-            .view_dir = normalize(intersection.point - r.origin())};
-        registry.view<components::DirectionalLight>().each(
-            [&ld](auto entity, auto &dirlight) { dirlight.UpdateColor(ld); });
-        registry.view<components::Transform, components::SpotLight>().each(
-            [&ld](auto entity, auto &transform, auto &spot_light) { spot_light.UpdateColor(transform, ld); });
-        registry.view<components::Transform, components::PointLight>().each(
-            [&ld](auto entity, auto &transform, auto &point_light) { point_light.UpdateColor(transform, ld); });
+            .view_dir = normalize(intersection.point - ray.origin())};
+        directional_lights.each([&ld](const auto, auto& dirlight) {dirlight.UpdateColor(ld); });
+        point_lights.each([&ld](const auto, auto const& point_light, auto const& transform) { point_light.UpdateColor(transform, ld); });
+        spot_lights.each([&ld](const auto, auto const& spot_light, auto const& transform) { spot_light.UpdateColor(transform, ld); });
         ivec3 color{ 255.99f * ld.color };
         color %= 256;
         color.r <<= 16;
         color.g <<= 8;
 
-        bitmap[size_t(j) * window_size.x + i] = color.r | color.g | color.b;
+        bitmap[size_t(j) * bitmap_size.x + i] = color.r | color.g | color.b;
       }
     }
   }
 
-  math::Intersection Scene::FindIntersection(math::Ray const &ray)
-  {
-    math::Intersection nearest;
-    nearest.reset();
-    for (auto &&[entity, transform] : registry.group<components::Transform, components::Sphere>().each())
-    {
-      if(components::Sphere::CheckIntersection(transform, nearest, ray))
-      {
-          return nearest;
-      }
-    }
-    for (auto &&[entity, transform, plane] : registry.view<components::Transform, components::Plane>().each())
-    {
-      if(plane.CheckIntersection(transform, nearest, ray))
-      {
-          return nearest;
-      }
-    }
-    for (auto &&[entity, transform] : registry.view<components::Transform, components::Cube>().each())
-    {
-      if(components::Cube::CheckIntersection(transform, nearest, ray))
-      {
-          return nearest;
-      }
-    }
-    return nearest;
-  }
 } // namespace engine
--- engine/misc/scene.hpp
+++ engine/misc/scene.hpp
@@ -2,11 +2,15 @@
 
 #include "core/bitmap-window.hpp"
 #include "math/ray.hpp"
+#include "components/camera.hpp"
+#include "components/plane.hpp"
 #include "components/sphere.hpp"
+#include "components/cube.hpp"
 #include "components/directional-light.hpp"
 #include "components/point-light.hpp"
 #include "components/spot-light.hpp"
-#include "components/camera.hpp"
+
+#include "entt/entt.hpp"
 
 namespace engine
 {
@@ -21,6 +25,7 @@ namespace engine
         math::Intersection FindIntersection(math::Ray const& ray);
         
         bool update_scene{true};
+        
         entt::registry registry;
     };
 } // namespace engine
