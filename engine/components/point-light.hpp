93fff9789e12022869ab2af80da82b33e0e99725
malatindez <53783400+malatindez@users.noreply.github.com>
2022-05-19 13:34:30 +0300

    Update point-light.hpp

--- engine/components/point-light.hpp
+++ engine/components/point-light.hpp
@@ -18,7 +18,7 @@ namespace engine::components
         } attenuation;
 
         
-        void UpdateColor(Transform &transform, LightData &light_data) const
+        inline void UpdateColor(Transform const &transform, LightData &light_data) const
         {
             math::vec3 const light_direction = - light_data.point + transform.position;
             float const t = math::dot(light_data.normal, light_direction);
@@ -28,20 +28,20 @@ namespace engine::components
                 return;
             }
             float distance = math::length(light_direction);
-            float attenuation = 1.0f /
-                                (this->attenuation.constant +
-                                 this->attenuation.linear * distance +
-                                 this->attenuation.quadratic * distance * distance);
+            float attenuation_ = 1.0f /
+                                (attenuation.constant +
+                                 attenuation.linear * distance +
+                                 attenuation.quadratic * distance * distance);
 
             math::vec3 const reflect_dir = math::normalize(2.0f * 
                     math::dot(light_data.normal, light_direction) * 
                     (light_data.normal - light_direction));
             
-            float diffuse = attenuation * diffuse_intensity * t;
+            float diffuse = diffuse_intensity * t;
             float u = dot(light_data.view_dir, reflect_dir);
-            float specular = attenuation* specular_intensity* static_cast<float>(std::pow(std::max(u, 0.0f), 32));
+            float specular = specular_intensity* static_cast<float>(std::pow(std::max(u, 0.0f), 32));
             
-            light_data.color += color * (ambient_intensity + diffuse + specular);
+            light_data.color += color * attenuation_ * (ambient_intensity + diffuse + specular);
             
         }
     };
