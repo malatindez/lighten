0031a2c0a390f8a86ca4e255a2e4835b06868c3b
malatindez <axenance@gmail.com>
2022-09-02 17:40:23 +0300

    skybox jiggle fix

--- runtime_directory/assets/shaders/skybox/skybox.hlsl
+++ runtime_directory/assets/shaders/skybox/skybox.hlsl
@@ -1,5 +1,4 @@
 #include "../globals/vs.hlsli"
-cbuffer CameraModel : register(b1) { row_major matrix g_camera_model; }
 struct VertexIn {
   uint vertex : SV_VertexID;
 };
@@ -16,13 +15,16 @@ VertexOut vs_main(VertexIn vin) {
   if (vin.vertex == 0) { vertex_coords = float2(0, 1); } 
   else if (vin.vertex == 1) { vertex_coords = float2(0, 0); } 
   else if (vin.vertex == 2) { vertex_coords = float2(1, 0); }
-  float4 bl4 = mul(float4(-1, -1, 1, 1), inv_view_projection);
-  float4 br4 = mul(float4(1, -1, 1, 1), inv_view_projection);
-  float4 tl4 = mul(float4(-1, 1, 1, 1), inv_view_projection);
+  float4x4 g_camera_inv_view_projection = inv_view;
+  g_camera_inv_view_projection[3][0] = g_camera_inv_view_projection[3][1] = g_camera_inv_view_projection[3][2] = 0;
+  g_camera_inv_view_projection = mul(inv_projection,g_camera_inv_view_projection);
+  float4 bl4 = mul(float4(-1, -1, 1, 1), g_camera_inv_view_projection);
+  float4 br4 = mul(float4(1, -1, 1, 1), g_camera_inv_view_projection);
+  float4 tl4 = mul(float4(-1, 1, 1, 1), g_camera_inv_view_projection);
   float4 right4 = br4 - bl4;
   float4 up4 = tl4 - bl4;
   float4 ray = bl4 + right4 * vertex_coords.x * 2 + up4 * vertex_coords.y * 2;
-  vout.PosL = ray.xyz / ray.w - inv_view[3].xyz;
+  vout.PosL = ray.xyz / ray.w;
   vout.PosH = float4(vertex_coords * 4 - float2(1, 1), 0, 1);
   return vout;
 }
